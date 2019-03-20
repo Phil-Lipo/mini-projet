fe5de8989514c99390fa00c5fcbe8dada682373c
Phil-Lipo <padioleau.p@gmail.com>
2019-03-20 13:45:00 +0100

    Ajout composant calcul heure

--- index.js
+++ index.js
@@ -17,11 +17,53 @@ class App extends Component {
         <Hello name={this.state.name} />
         <p>
           <ComposantOne />
+          <ComposantTwo />
         </p>
       </div>
     );
   }
 }
+
+class ComposantTwo extends Component {
+  constructor() {
+    super();
+    this.state = {
+      heure: null,
+      minutes: null,
+      nombre: null
+    };
+  }
+  handleChangeHeure(e){
+    console.log("handleChangeHeure"+e.target.value);
+    this.setState({heure: e.target.value},this.calculNombre);
+  }
+
+  handleChangeMinutes(e){
+    console.log("handleChangeMinutes"+e.target.value);
+    this.setState({minutes: e.target.value},this.calculNombre);
+  }
+
+  calculNombre(){
+    console.log(this.state.heure+"//"+this.state.minutes)
+    let minutes = (this.state.minutes == null)? 0 : this.state.minutes;
+    let heures = (this.state.heure == null)? 0 : this.state.heure;
+    this.setState({nombre: heures+","+minutes});
+  }
+  render() {
+    return (
+      <div>
+        <h2>Calcul heure en nombre</h2>
+        <p>
+          heure :  <input type="text" id="heure" maxlength={2} size={2} value={this.state.heure} onChange={this.handleChangeHeure.bind(this)}/> : <input type="text" id="heure" maxlength={2} maxlenght={2} size={2} value={this.state.minutes} onChange={this.handleChangeMinutes.bind(this)} /><br/>
+          decimal :<input type="text" id="nombre" value={this.state.nombre} onChange={this.handleChangeMinutes.bind(this)}/><br/>
+
+        </p>
+        <hr/>
+      </div>
+    );
+  }
+}
+
 
 class ComposantOne extends Component {
   constructor() {
@@ -51,6 +93,7 @@ class ComposantOne extends Component {
   render() {
     return (
       <div>
+        <hr/>
         <h2>Calcul aire d'un rectangle</h2>
         <p>
         Longueur :  <input type="text" id="longueur" value={this.state.longueur} onChange={this.handleChangeLongueur.bind(this)} on/><br/>
@@ -58,11 +101,11 @@ class ComposantOne extends Component {
           Aire :<input type="text" value={this.state.aire}/><br/>
 
         </p>
+        <hr/>
       </div>
     );
   }
 }
-
 
 
 render(<App />, document.getElementById('root'));
